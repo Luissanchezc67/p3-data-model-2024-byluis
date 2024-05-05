d539e61bc4b51a327944d899dd33c5cba7bfc970
Luissanchezc67 <luissanchezc67@gmail.com>
2024-05-05 19:18:54 +0200

    probando datos en una tabla

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const result = await prisma.pacient.create({
+    data:{
+        pacient: "Andy Josue",
+        address: "la sagrera",
+        phonenum: "623673823",
+        DNIPass: "4523782K",
+        edad: "24",
+        gender:"varon",
+        record:"alergic aines"
+
+    }
+})
+
+console.log("You created pacient:", result);
